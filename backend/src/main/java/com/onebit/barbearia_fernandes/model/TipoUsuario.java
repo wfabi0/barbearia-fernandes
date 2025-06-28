e3f7c18ef7c56b7372ea57f20d477d4d239c5d65
Carlos Eduardo de Melo <154885531+CarlosEduardodeMelo17@users.noreply.github.com>
2025-06-28 12:23:33 -0300

    Criado a entidade Usuario

---
+++ backend/src/main/java/com/onebit/barbearia_fernandes/model/TipoUsuario.java
@@ -0,0 +1,5 @@
+package com.onebit.barbearia_fernandes.model;
+
+public enum TipoUsuario {
+    CLIENTE, BARBEIRO
+}
---
+++ backend/src/main/java/com/onebit/barbearia_fernandes/model/Usuario.java
@@ -0,0 +1,125 @@
+package com.onebit.barbearia_fernandes.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "nome")
+    private String nomeUsuario;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "perfil")
+    private TipoUsuario perfil = TipoUsuario.CLIENTE;
+
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdat;
+
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long userId, String nomeUsuario, String email, String telefone, String senha, TipoUsuario perfil, LocalDateTime createdat, LocalDateTime updatedAt) {
+        this.userId = userId;
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.perfil = perfil;
+        this.createdat = createdat;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(TipoUsuario perfil) {
+        this.perfil = perfil;
+    }
+
+    public LocalDateTime getCreatedat() {
+        return createdat;
+    }
+
+    public void setCreatedat(LocalDateTime createdat) {
+        this.createdat = createdat;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        createdat = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}
