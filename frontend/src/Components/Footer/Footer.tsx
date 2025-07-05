686b191c0ea53d78ba65d784ff2e8508e411f884
Gabriel Káicon <gkaicon2@gmail.com>
2025-07-05 18:29:29 -0300

    feature: finalização da tela de cadastro cliente e correções de componentes

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="mt-5 bottom-0 bg-black text-white text-center w-full">
+        <footer className="static mt-5 bottom-0 bg-black text-white text-center w-full">
             <p>&copy; Desenvolvido por Trainees da Onebit JR 2025.</p>
         </footer>
     )
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,40 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import Body from "../Components/Body/Body";
 import "./globals.css";
 
-import NavBar from '../Components/NavBar/NavBar';
-import Footer from '../Components/Footer/Footer';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Barbearia Fernandes",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        style={{ backgroundImage: "url('./bg-50.png')", backgroundSize: "cover" }}>
-        <NavBar></NavBar>
-        <main>
-          {children}
-        </main>
-        <Footer></Footer>
-      </body>
+    <html lang="pt-BR">
+      <Body>
+        {children}
+      </Body>
+
     </html>
   );
 }
