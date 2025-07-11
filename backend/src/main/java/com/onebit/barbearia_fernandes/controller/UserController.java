695d2b67fffdf785c0a6d730ff33cdf809f535bb
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2025-07-11 20:52:13 -0300

    chore: removido imports não utilizados

--- backend/src/main/java/com/onebit/barbearia_fernandes/controller/UserController.java
+++ backend/src/main/java/com/onebit/barbearia_fernandes/controller/UserController.java
@@ -2,13 +2,12 @@ package com.onebit.barbearia_fernandes.controller;
 
 import com.onebit.barbearia_fernandes.dto.CreateUserDto;
 import com.onebit.barbearia_fernandes.dto.UserResponseDto;
-import com.onebit.barbearia_fernandes.model.Usuario;
 import com.onebit.barbearia_fernandes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
